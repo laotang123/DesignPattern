10d79b3bce38d050f371c6706c7ae077af40ff53
liujunfeng <and1678354579@gmail.com>
2020-06-15 09:57:28 +0800

    微调

---
+++ src/main/java/com/ljf/pattern/factory/ReflectFactoryPattern.java
@@ -0,0 +1,37 @@
+package com.ljf.pattern.factory;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author ：ljf
+ * @date ：2020/6/15 9:04
+ * @description：反射实现工厂模式
+ * @modified By：
+ * @version: $ 1.0
+ */
+public class ReflectFactoryPattern {
+
+
+    private static Map<String, Class> registeredProducts = new HashMap<String, Class>();
+
+    public static void registerVehicle(String vehicleID, Class vehicleClass) {
+        registeredProducts.put(vehicleID, vehicleClass);
+    }
+
+    public static Vehicle createVehicle(String type) throws IllegalAccessException, InstantiationException {
+        Class productClass = registeredProducts.get(type);
+        return (Vehicle) productClass.newInstance();
+    }
+
+    public static void main(String[] args) {
+        ReflectFactoryPattern.registerVehicle("com.ljf.pattern.factory.Car",Car.class);
+        try {
+            ReflectFactoryPattern.createVehicle("com.ljf.pattern.factory.Car").running();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/com/ljf/pattern/factory/StaticFactoryPattern.java
@@ -0,0 +1,71 @@
+package com.ljf.pattern.factory;
+
+/**
+ * @author ：ljf
+ * @date ：2020/6/14 22:31
+ * @description：TODO
+ * @modified By：
+ * @version: $ 1.0
+ */
+
+/**
+ * 枚举类实现简单静态工厂模式
+ */
+abstract class Vehicle {
+    abstract void running();
+    abstract Vehicle newInstance();
+}
+
+class Bike extends Vehicle {
+    @Override
+    void running() {
+        System.out.println("bike is running!");
+    }
+
+    Vehicle newInstance() {
+        return new Bike();
+    }
+}
+
+class Car extends Vehicle {
+    @Override
+    void running() {
+        System.out.println("car is running!");
+    }
+
+    Vehicle newInstance() {
+        return new Car();
+    }
+}
+
+class Truck extends Vehicle {
+    @Override
+    void running() {
+        System.out.println("truck is running!");
+    }
+
+    Vehicle newInstance() {
+        return new Truck();
+    }
+}
+
+
+public class StaticFactoryPattern {
+    public enum VehicleType {
+        BIKE, CAR, TRUCK
+    }
+
+    public static Vehicle create(VehicleType type) {
+        if (type == VehicleType.BIKE) return new Bike();
+        else if (type == VehicleType.CAR) return new Car();
+        else if (type == VehicleType.TRUCK) return new Truck();
+        else return null;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(StaticFactoryPattern.create(VehicleType.BIKE));
+        System.out.println(StaticFactoryPattern.create(VehicleType.CAR));
+        System.out.println(StaticFactoryPattern.create(VehicleType.TRUCK));
+        StaticFactoryPattern.create(VehicleType.TRUCK).running();
+    }
+}
