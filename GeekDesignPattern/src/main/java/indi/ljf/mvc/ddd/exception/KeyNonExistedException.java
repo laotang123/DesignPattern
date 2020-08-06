1e7c279321c3658acc149bf4052bcc718883de05
liujunfeng <and1678354579@gmail.com>
2020-08-06 10:08:34 +0800

    添加key不存在，错误匹配exception

---
+++ GeekDesignPattern/src/main/java/indi/ljf/mvc/ddd/exception/KeyNonExistedException.java
@@ -0,0 +1,30 @@
+package indi.ljf.mvc.ddd.exception;
+
+/**
+ * @author ：ljf
+ * @date ：2020/8/6 10:03
+ * @description：map key不存在异常类型
+ * @modified By：
+ * @version: $ 1.0
+ */
+public class KeyNonExistedException extends Exception{
+    public KeyNonExistedException() {
+        super();
+    }
+
+    public KeyNonExistedException(String message) {
+        super(message);
+    }
+
+    public KeyNonExistedException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public KeyNonExistedException(Throwable cause) {
+        super(cause);
+    }
+
+    protected KeyNonExistedException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+}
