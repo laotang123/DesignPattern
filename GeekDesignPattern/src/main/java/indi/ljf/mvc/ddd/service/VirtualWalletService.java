65a30366610c4c04d0ee0204e2c162a9c7bcaf38
liujunfeng <and1678354579@gmail.com>
2020-08-06 10:08:54 +0800

    添加抽象service

---
+++ GeekDesignPattern/src/main/java/indi/ljf/mvc/ddd/service/VirtualWalletService.java
@@ -0,0 +1,50 @@
+package indi.ljf.mvc.ddd.service;
+
+import indi.ljf.mvc.ddd.dao.TransferRecord;
+
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ * @author ：ljf
+ * @date ：2020/8/6 9:36
+ * @description：虚拟钱包服务
+ * @modified By：
+ * @version: $ 1.0
+ */
+public abstract class VirtualWalletService {
+    /**
+     * 获取指定钱包id的钱包金额
+     * @param walletId
+     * @return
+     */
+    public abstract BigDecimal getBalance(BigInteger walletId);
+
+    /**
+     * 根据钱包Id，返回对应的转账记录；TODO：实现多种形式的转账记录查询：Byid，byTime
+     * @param walletId
+     * @return
+     */
+    public abstract TransferRecord getTransferRecord(BigInteger walletId);
+    /**
+     * 取现业务
+     * @param walletId：取钱的账户
+     * @param amount：取钱的金额
+     */
+    public abstract void withdraw(BigInteger walletId, BigDecimal amount);
+
+    /**
+     * 充值业务
+     * @param walletId：待充值钱包id
+     * @param amount：充值金额
+     */
+    public abstract void recharge(BigInteger walletId,BigDecimal amount);
+
+    /**
+     * 转账业务
+     * @param fromWalletId：转出钱包id
+     * @param toWalletId：转入钱包id
+     * @param amount：转账金额
+     */
+    public abstract void transfer(BigInteger fromWalletId,BigInteger toWalletId,BigDecimal amount);
+}
